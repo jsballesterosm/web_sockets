2f797054c54205fbe0a6e441fb855bd3180e6a49
Juan Sebastian Ballesteros <jballesteros@octansit.com>
2021-05-06 21:14:53 -0500

    sockets get info

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ Project Referent
 
 ## Getting Started
 
+Instal dependences
+- npm install
+
 This project is a starting point for a Flutter application.
 
 - npm start => production
--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,22 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+
+const Bands = require('../models/bands');
+const bands = new Bands();
+
+bands.addBand(new Band('Pantera'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Metallica'));
+
 // Mensajes de Sockets
 io.on('connection', client => {
-    console.log('Cliente conectado');
+    console.log('Cliente conectado', new Date());
+
+    client.emit('active-bands', bands.getBands());
+
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        console.log('Cliente desconectado', new Date());
     });
 
     client.on('mensaje', (payload) => {
@@ -11,4 +24,28 @@ io.on('connection', client => {
 
         io.emit('mensaje', { admin: 'nuevo mensajes' });
     });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        // enviamos a todo el mundo el cambio de informaicon
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        // enviamos a todo el mundo el cambio de informaicon
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        // enviamos a todo el mundo el cambio de informaicon
+        io.emit('active-bands', bands.getBands());
+    });
+
+    /*client.on('emitir-mensaje', (payload) => {
+        // io.emit('nuevo-mensaje', payload); // emite el mensaje a todos
+        client.broadcast.emit('nuevo-mensaje', payload); // emite el mensaje a todos menos al que lo emitio
+    });*/
 });
